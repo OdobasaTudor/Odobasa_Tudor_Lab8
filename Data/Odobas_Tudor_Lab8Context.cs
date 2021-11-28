d71cb46782a0ea0179a8284727595952f62a54d9
TUDOR ODOBASA <odobasa.tudor174169@student.ubbcluj.ro>
2021-11-28 21:02:31 +0200

    Add project files.

---
+++ Data/Odobas_Tudor_Lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Odobas_Tudor_Lab8.Models;
+
+namespace Odobas_Tudor_Lab8.Data
+{
+    public class Odobas_Tudor_Lab8Context : DbContext
+    {
+        public Odobas_Tudor_Lab8Context (DbContextOptions<Odobas_Tudor_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Odobas_Tudor_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Odobas_Tudor_Lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Pages/Publishers/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Odobas_Tudor_Lab8.Data;
+using Odobas_Tudor_Lab8.Models;
+
+namespace Odobas_Tudor_Lab8.Pages.Publishers
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Odobas_Tudor_Lab8.Data.Odobas_Tudor_Lab8Context _context;
+
+        public DetailsModel(Odobas_Tudor_Lab8.Data.Odobas_Tudor_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public Publisher Publisher { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Publisher = await _context.Publisher.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Publisher == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
